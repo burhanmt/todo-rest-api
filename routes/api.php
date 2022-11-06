355fc9d141109ccd13bd891e7dc697d5570a0ecf
burhanmt <burhanmt@icloud.com>
2022-11-06 04:33:16 +0000

    TaskController resource was added.

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+/**
+ * User password credential grant type authentication check
+ */
+Route::middleware(['check-api-header', 'auth:api'])->group(function () {
+    Route::prefix('v1')->group(static function () {
+       Route::resource('task', TaskController::class);
+    });
 });
+
