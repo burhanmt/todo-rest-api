e9ef17fb6a5f467b29a5290749232d6923fca75e
burhanmt <burhanmt@icloud.com>
2022-11-06 16:11:43 +0000

    Authentication error message improvements

--- routes/api.php
+++ routes/api.php
@@ -23,7 +23,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['check-api-header', 'api'])->group(function () {
     Route::prefix('v1')->group(static function () {
         Route::post('/user/register', [UserController::class, 'register'])->name('user.register');
-        Route::post('/user/login', [UserController::class, 'login'])->name('user.login');
     });
 });
 
