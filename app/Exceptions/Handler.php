c68733fcca301ccf0daab6a31d9a235c66bd7f22
burhanmt <burhanmt@icloud.com>
2022-11-06 04:21:20 +0000

    I customised the error handling's payload fields, name conventions

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,16 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Contracts\Support\Responsable;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Routing\Router;
+use Illuminate\Support\Collection;
+use Illuminate\Validation\ValidationException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -46,5 +55,91 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    /**
+     * We override the invalidJson method.
+     *
+     * @param Request $request
+     * @param ValidationException $exception
+     * @return JsonResponse
+     */
+    protected function invalidJson($request, ValidationException $exception)
+    {
+        $errors = (new Collection($exception->validator->errors()))->map(
+            function ($error, $key) use ($exception) {
+                return [
+                    'message' => $error[0],
+                    'error_code' => $exception->status,
+                ];
+            }
+        )->values();
+
+        return response()->json(['errors' => $errors], $exception->status);
+    }
+
+    /**
+     * We override the render method.
+     *
+     * @param Request $request
+     * @param Throwable $e
+     * @return false|JsonResponse|Response|\Symfony\Component\HttpFoundation\Response
+     */
+    public function render($request, Throwable $e)
+    {
+        if (method_exists($e, 'render') && $response = $e->render($request)) {
+            return Router::toResponse($request, $response);
+        }
+
+        if ($e instanceof Responsable) {
+            return $e->toResponse($request);
+        }
+
+        $e = $this->prepareException($e);
+        if ($e instanceof HttpResponseException) {
+            return $e->getResponse();
+        }
+
+        if ($e instanceof AuthenticationException) {
+            return $this->unauthenticated($request, $e);
+        }
+
+        if ($e instanceof ValidationException) {
+            return $this->convertValidationExceptionToResponse($e, $request);
+        }
+
+        return $request->expectsJson()
+            ? $this->prepareJsonResponse($request, $e) : $this->prepareResponse($request, $e);
+    }
+
+    /**
+     * We override the prepareJsonResponse method
+     *
+     * @param Request $request
+     * @param Throwable $e
+     * @return JsonResponse
+     */
+    protected function prepareJsonResponse($request, Throwable $e)
+    {
+        $errorMessage = $e->getMessage();
+        if ($this->isHttpException($e) && $e->getStatusCode() === 404) {
+            $errorMessage = 'The resource is not Found!';
+        }
+
+        return response()->json([
+            'message' => $errorMessage,
+            'error_code' => $this->isHttpException($e) ? $e->getStatusCode() : 500,
+        ], $this->isHttpException($e) ? $e->getStatusCode() : 500);
+    }
+
+    /**
+     * We override the unauthenticated method.
+     */
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        return response()->json([
+            'message' => 'You are not authenticated',
+            'error_code' => 403,
+        ], 403);
     }
 }
